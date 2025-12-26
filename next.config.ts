256b8a29ecca65c34ab60b954d99730badad6b90
RajRaiyani <rpraiyani7@gmail.com>
2025-12-26 12:06:25 +0530

    refactor

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: "3007",
         pathname: "/files/**",
       },
+      {
+        protocol: "https",
+        hostname: "server.jeeko.com",
+        port: "443",
+        pathname: "/**",
+      },
     ],
     // Allow unoptimized images in development to bypass private IP restrictions
     unoptimized: process.env.NODE_ENV === "development",
